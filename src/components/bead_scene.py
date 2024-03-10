4576f2ba4579f67c42d779cd57a55e1112ccf5a1
zherberger <zherberger@gmail.com>
2024-03-10 17:19:29 -0400

    Show bead color count. Automatically select bead tab after loading an image.

--- src/components/bead_scene.py
+++ src/components/bead_scene.py
@@ -12,6 +12,7 @@ def get_hex_key(rgb):
 
 def get_closest_bead(rgb):
     min_distance = 100.0
+    closest_match = None
 
     for bead in PERLER_BEADS:
         distance = compute_color_distance(srgb_to_lab(bead["rgb"]), srgb_to_lab(rgb))
@@ -33,16 +34,20 @@ class BeadWorkflow(QtWidgets.QWidget):
         self.bead_legend = None
         self.layout = QtWidgets.QHBoxLayout(self)
 
-        self.tabs = QtWidgets.QTabWidget()
         self.original_view = BeadView()
         self.bead_view = BeadView()
-        self.tabs.addTab(self.original_view, "Original")
-        self.tabs.addTab(self.bead_view, "Beads")
-        self.layout.addWidget(self.tabs)
 
-        self.bead_matches_obtained.connect(self.update_legend)
-
+        bead_widget = QtWidgets.QWidget()
+        bead_widget_layout = QtWidgets.QHBoxLayout(bead_widget)
+        bead_widget_layout.addWidget(self.bead_view)
+        bead_widget_layout.addWidget(BeadLegend(self.bead_matches_obtained))
+        bead_widget.setLayout(bead_widget_layout)
         image_loaded.connect(self.load_image)
+
+        self.tabs = QtWidgets.QTabWidget()
+        self.tabs.addTab(self.original_view, "Original")
+        self.tabs.addTab(bead_widget, "Beads")
+        self.layout.addWidget(self.tabs)
 
     @QtCore.Slot()
     def load_image(self, file_name):
@@ -61,8 +66,10 @@ class BeadWorkflow(QtWidgets.QWidget):
             for pixel in row:
                 bead_data[row_num].append(self.bead_matches.get(get_hex_key(pixel))["rgb"])
             row_num += 1
-        self.bead_matches_obtained.emit(self.bead_matches)
+
         self.bead_view.draw_image(bead_data)
+        self.bead_matches_obtained.emit(self.bead_matches)
+        self.tabs.setCurrentIndex(1)
 
     def generate_bead_matches(self):
         bead_matches = {}
@@ -74,31 +81,46 @@ class BeadWorkflow(QtWidgets.QWidget):
 
                 if bead_matches.get(key) is None:
                     bead_matches[key] = get_closest_bead(rgb)
+                    bead_matches[key]["count"] = 1
+                else:
+                    bead_matches[key]["count"] += 1
 
         return bead_matches
 
-    def update_legend(self, bead_matches):
+
+class BeadLegend(QtWidgets.QWidget):
+    def __init__(self, bead_matches_obtained):
+        super().__init__()
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.setAlignment(QtCore.Qt.AlignmentFlag.AlignTop)
+        self.layout.setContentsMargins(0, 0, 0, 0)
+        self.layout.setSpacing(0)
+        self.bead_matches_obtained = bead_matches_obtained
+        self.bead_matches_obtained.connect(self.populate_legend)
+
+    @QtCore.Slot()
+    def populate_legend(self, bead_matches):
         values = bead_matches.values()
-        if self.bead_legend is not None:
-            self.layout.removeWidget(self.bead_legend)
-        bead_legend = QtWidgets.QWidget()
-        bead_legend.layout = QtWidgets.QVBoxLayout(bead_legend)
 
         for bead in values:
-            widget = QtWidgets.QWidget()
-            widget.layout = QtWidgets.QHBoxLayout(widget)
+            legend_entry = QtWidgets.QWidget()
+            legend_entry_layout = QtWidgets.QHBoxLayout(legend_entry)
+            legend_entry_layout.setSpacing(0)
+            legend_entry_layout.setContentsMargins(0, 0, 0, 0)
+            legend_entry_layout.setAlignment(QtCore.Qt.AlignmentFlag.AlignLeft)
+            legend_entry.layout = legend_entry_layout
+
             pixmap = QtGui.QPixmap(15, 15)
             color = QtGui.QColor.fromRgb(bead["rgb"][0], bead["rgb"][1], bead["rgb"][2])
             pixmap.fill(color)
-            name_label = QtWidgets.QLabel(bead["name"])
+            name_label = QtWidgets.QLabel(f"{bead['name']} ({bead['count']})")
+            name_label.setAlignment(QtCore.Qt.AlignmentFlag.AlignLeft)
             color_label = QtWidgets.QLabel()
             color_label.setPixmap(pixmap)
-            widget.layout.addWidget(color_label)
-            widget.layout.addWidget(name_label)
-            bead_legend.layout.addWidget(widget)
-
-        self.bead_legend = bead_legend
-        self.layout.addWidget(self.bead_legend)
+            color_label.setMargin(6)
+            legend_entry.layout.addWidget(color_label)
+            legend_entry.layout.addWidget(name_label)
+            self.layout.addWidget(legend_entry)
 
 
 class BeadView(QtWidgets.QGraphicsView):
