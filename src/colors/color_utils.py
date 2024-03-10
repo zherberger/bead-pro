e87d869771d6710a45e66c6d0d50440056a76f62
zherberger <zherberger@gmail.com>
2024-03-10 11:47:03 -0400

    Some reformatting

--- src/colors/color_utils.py
+++ src/colors/color_utils.py
@@ -15,31 +15,31 @@ Y_W = 1.0
 Z_W = 1.088840
 
 
-def DEGREES_TO_RADIANS(deg):
+def degrees_to_radians(deg):
     return 2 * np.pi * deg / 360.0
 
 
-def RADIANS_TO_DEGREES(rad):
+def radians_to_degrees(rad):
     return rad * 360.0 / (2 * np.pi)
 
 
 def srgb_to_xyz(srgb):
     def gamma_expansion(value255):
-        if (value255 <= 10):
+        if value255 <= 10:
             return float(value255) / 3294.6
         else:
             return ((float(value255) + 14.025) / 269.025) ** 2.4
 
-    R = gamma_expansion(srgb[0])
-    G = gamma_expansion(srgb[1])
-    B = gamma_expansion(srgb[2])
+    r = gamma_expansion(srgb[0])
+    g = gamma_expansion(srgb[1])
+    b = gamma_expansion(srgb[2])
 
-    return np.matmul(SRGB_XYZ_TRANSFORMATION_MATRIX, [R, G, B])
+    return np.matmul(SRGB_XYZ_TRANSFORMATION_MATRIX, [r, g, b])
 
 
 def xyz_to_lab(xyz):
     def f(v):
-        if (v > EPSILON):
+        if v > EPSILON:
             return v ** (1.0 / 3.0)
         else:
             return (KAPPA * v + 16.0) / 116.0
@@ -60,8 +60,8 @@ def srgb_to_lab(srgb):
 
 
 def lab_to_lch(lab):
-    h = RADIANS_TO_DEGREES(np.arctan2(lab[2], lab[1]))
-    if (h < 0.0):
+    h = radians_to_degrees(np.arctan2(lab[2], lab[1]))
+    if h < 0.0:
         h = 360.0 + h
 
     return [
@@ -71,66 +71,67 @@ def lab_to_lch(lab):
     ]
 
 
+def calc_delta_h_prime(h1_prime, h2_prime):
+    abs_diff = np.abs(h1_prime - h2_prime)
+
+    if abs_diff <= 180.0:
+        return h2_prime - h1_prime
+    elif abs_diff > 180.0 and h2_prime <= h1_prime:
+        return h2_prime - h1_prime + 360.0
+    else:
+        return h2_prime - h1_prime - 360.0
+
+
 def compute_color_distance(lab1, lab2):
     lch1 = lab_to_lch(lab1)
     lch2 = lab_to_lch(lab2)
-    L_BAR = (lab1[0] + lab2[0]) / 2.0
-    DELTA_L = lab1[0] - lab2[0]
+    l_bar = (lab1[0] + lab2[0]) / 2.0
+    delta_l = lab1[0] - lab2[0]
 
     def calc_a_prime(a, cbar):
         return a * (1 + 0.5 * (1 - np.sqrt(cbar ** 7 / (cbar ** 7 + 25.0 ** 7))))
 
-    C1 = lch1[1]
-    C2 = lch2[1]
-    C_BAR = (C1 + C2) / 2.0
-    a1_prime = calc_a_prime(lab1[1], C_BAR)
-    a2_prime = calc_a_prime(lab2[1], C_BAR)
-    C1_PRIME = np.hypot(a1_prime, lab1[2])
-    C2_PRIME = np.hypot(a2_prime, lab2[2])
-    C_BAR_PRIME = (C1_PRIME + C2_PRIME) / 2.0
-    DELTA_C = C2_PRIME - C1_PRIME
+    c1 = lch1[1]
+    c2 = lch2[1]
+    c_bar = (c1 + c2) / 2.0
+    a1_prime = calc_a_prime(lab1[1], c_bar)
+    a2_prime = calc_a_prime(lab2[1], c_bar)
+    c1_prime = np.hypot(a1_prime, lab1[2])
+    c2_prime = np.hypot(a2_prime, lab2[2])
+    c_bar_prime = (c1_prime + c2_prime) / 2.0
+    delta_c = c2_prime - c1_prime
 
-    h1_prime = RADIANS_TO_DEGREES(np.arctan2(lab1[2], a1_prime)) % 360.0
-    h2_prime = RADIANS_TO_DEGREES(np.arctan2(lab2[2], a2_prime)) % 360.0
-
-    def calc_delta_h_prime(h1_prime, h2_prime):
-        abs_diff = np.abs(h1_prime - h2_prime)
-
-        if (abs_diff <= 180.0):
-            return h2_prime - h1_prime
-        elif (abs_diff > 180.0 and h2_prime <= h1_prime):
-            return h2_prime - h1_prime + 360.0
-        else:
-            return h2_prime - h1_prime - 360.0
+    h1_prime = radians_to_degrees(np.arctan2(lab1[2], a1_prime)) % 360.0
+    h2_prime = radians_to_degrees(np.arctan2(lab2[2], a2_prime)) % 360.0
 
     delta_h_prime = calc_delta_h_prime(h1_prime, h2_prime)
-    DELTA_H = 2 * np.sqrt(C1_PRIME * C2_PRIME) * np.sin(DEGREES_TO_RADIANS(delta_h_prime / 2.0))
+    delta_h = 2 * np.sqrt(c1_prime * c2_prime) * np.sin(degrees_to_radians(delta_h_prime / 2.0))
 
-    H_BAR_PRIME = 0.0
+    h_bar_prime = 0.0
     abs_diff = np.abs(h1_prime - h2_prime)
 
-    if (abs_diff <= 180.0):
-        H_BAR_PRIME = (h1_prime + h2_prime) / 2.0
-    elif (abs_diff > 180.0 and h1_prime + h2_prime < 360.0):
-        H_BAR_PRIME = (h1_prime + h2_prime + 360.0) / 2.0
+    if abs_diff <= 180.0:
+        h_bar_prime = (h1_prime + h2_prime) / 2.0
+    elif abs_diff > 180.0 and h1_prime + h2_prime < 360.0:
+        h_bar_prime = (h1_prime + h2_prime + 360.0) / 2.0
     else:
-        H_BAR_PRIME = (h1_prime + h2_prime - 360.0) / 2.0
+        h_bar_prime = (h1_prime + h2_prime - 360.0) / 2.0
 
-    T = 1 \
-        - 0.17 * np.cos(DEGREES_TO_RADIANS(H_BAR_PRIME - 30.0)) \
-        + 0.24 * np.cos(DEGREES_TO_RADIANS(2 * H_BAR_PRIME)) \
-        + 0.32 * np.cos(DEGREES_TO_RADIANS(3 * H_BAR_PRIME + 6.0)) \
-        - 0.20 * np.cos(DEGREES_TO_RADIANS(4 * H_BAR_PRIME - 63.0))
+    t = 1 \
+        - 0.17 * np.cos(degrees_to_radians(h_bar_prime - 30.0)) \
+        + 0.24 * np.cos(degrees_to_radians(2 * h_bar_prime)) \
+        + 0.32 * np.cos(degrees_to_radians(3 * h_bar_prime + 6.0)) \
+        - 0.20 * np.cos(degrees_to_radians(4 * h_bar_prime - 63.0))
 
-    S_L = 1 + (0.015 * (L_BAR - 50.0) ** 2) / (np.sqrt(20.0 + (L_BAR - 50) ** 2))
-    S_C = 1 + 0.045 * C_BAR_PRIME
-    S_H = 1 + 0.015 * C_BAR_PRIME * T
-    R_T = -2 * np.sqrt(C_BAR_PRIME ** 7 / (C_BAR_PRIME ** 7 + 25.0 ** 7)) * np.sin(
-        DEGREES_TO_RADIANS(60 * np.exp(-1 * ((H_BAR_PRIME - 275) / 25) ** 2)))
+    s_l = 1 + (0.015 * (l_bar - 50.0) ** 2) / (np.sqrt(20.0 + (l_bar - 50) ** 2))
+    s_c = 1 + 0.045 * c_bar_prime
+    s_h = 1 + 0.015 * c_bar_prime * t
+    r_t = -2 * np.sqrt(c_bar_prime ** 7 / (c_bar_prime ** 7 + 25.0 ** 7)) * np.sin(
+        degrees_to_radians(60 * np.exp(-1 * ((h_bar_prime - 275) / 25) ** 2)))
 
-    return np.sqrt( \
-        (DELTA_L / S_L) ** 2 \
-        + (DELTA_C / S_C) ** 2 \
-        + (DELTA_H / S_H) ** 2 \
-        + R_T * (DELTA_C / S_C) * (DELTA_H / S_H)
+    return np.sqrt(
+        (delta_l / s_l) ** 2
+        + (delta_c / s_c) ** 2
+        + (delta_h / s_h) ** 2
+        + r_t * (delta_c / s_c) * (delta_h / s_h)
     )
